10ccbe71ca940c4a0118f653571b38c7c1848f7f
LuisEFranco94 <39841090+LuisEFranco94@users.noreply.github.com>
2024-02-05 00:06:15 -0500

    Adding dashboard project/study work from Streamlit ML Course

---
+++ Python_Streamlit_Training/Streamlit_Projects/Streamlit_ML_Course/Section_01_Introduction_To_Streamlit/main.py
@@ -0,0 +1,5 @@
+import streamlit as st 
+
+st.text("Hello Streamlit and World!")
+
+# Always rerun button will update the app instantly when making a code change
---
+++ Python_Streamlit_Training/Streamlit_Projects/Streamlit_ML_Course/Section_02_Streamlit_Fundamentals/02_Data_Display_Elements/main.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas as pd
+
+df = pd.read_csv("data/sample.csv", dtype="int")
+
+st.dataframe(df)
+st.write(df)
+
+st.table(df)
+
+st.metric(label="Population", value=900, delta=20, delta_color="normal")
+
+st.metric(label="Expenses", value=900, delta=-20, delta_color="inverse")
